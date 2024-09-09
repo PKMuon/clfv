bbad62db6062fbcde7969940fd0fa04e3c9d1311
lyazj <seeson@pku.edu.cn>
2024-09-10 01:38:16 +0800

    Fix energy beam energy

--- make_mueee_point.py
+++ make_mueee_point.py
@@ -8,19 +8,19 @@ zp_width_spline = MG5Width.MG5Width('data/zp_width_points.npz', masses_key='zp_m
 mueee_sc_card = MG5Card.MG5Card('cards/mueee_sc.dat')
 mueee_tc_card = MG5Card.MG5Card('cards/mueee_tc.dat')
 mueee_st_card = MG5Card.MG5Card('cards/mueee_st.dat')
-muon_energies = np.logspace(-1, 2, 31)
+muon_energies = np.logspace(-0.9, 2, 300)
 
 def make_mueee_point(zp_mass, muon_energy, card):
     print('make_mueee_point:', zp_mass, muon_energy, sep='\t')
-    zp_width = zp_width_spline.compute(zp_mass)
+    zp_width = zp_width_spline.compute(zp_mass)[0]
     xs, xs_unc, unit, nevent = card.run_xs({
         'nevent': 10000, 'zp_mass': zp_mass, 'zp_width': zp_width,
-        'electron_energy': 0.511, 'muon_energy': muon_energy,
+        'electron_energy': 0.511e-3, 'muon_energy': muon_energy,
     })
     unit = MG5Units.units[unit]
     xs *= unit
     xs_unc *= unit
-    print('make_mueee_point:', muon_energy, xs, xs_unc, sep='\t')
+    print('make_mueee_point:', zp_mass, muon_energy, xs, xs_unc, sep='\t')
     return xs, xs_unc
 
 def make_mueee_sc_point(args):
---
+++ make_mueee_points.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+./make_mueee_sc_points.py
+./make_mueee_tc_points.py
+./make_mueee_st_points.py
