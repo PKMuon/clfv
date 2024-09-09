7141cb2f754b653960f9d6d51b037da8a55babde
lyazj <seeson@pku.edu.cn>
2024-09-10 00:15:21 +0800

    Add plot_mueee_points.py

---
+++ plot_mueee_points.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+for channel in 'sc', 'tc':
+    mueee_points = np.load(f'data/mueee_{channel}_points.npz')
+    zp_masses = mueee_points['zp_masses']
+    muon_energies = mueee_points['muon_energies']
+    xses = mueee_points['xses']
+    for zp_mass, xs in zip(zp_masses, xses):
+        prefix = r'$m_{Z^\prime} = $' + f'{zp_mass} GeV, {channel[0]}-channel'
+        #plt.plot(muon_energies, xs, '.-', label=[prefix, prefix + ', uncertainty'])
+        plt.plot(muon_energies, xs[:,0], '.-', label=prefix)
+plt.xlabel('Muon energy [GeV]')
+plt.ylabel('Cross section [pb]')
+plt.xscale('log')
+plt.yscale('log')
+plt.legend()
+plt.grid()
+plt.tight_layout()
+plt.savefig('plot_mueee_points.pdf')
