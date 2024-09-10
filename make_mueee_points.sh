f001f00ef79abab1effe0c2bb57b13a1fe95e1fa
lyazj <seeson@pku.edu.cn>
2024-09-10 13:56:55 +0800

    Improve sh scripts

--- make_mueee_points.sh
+++ make_mueee_points.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-./make_mueee_sc_points.py
-./make_mueee_tc_points.py
-./make_mueee_st_points.py
+python3 -u make_mueee_sc_points.py
+python3 -u make_mueee_tc_points.py
+python3 -u make_mueee_st_points.py
