403939669afcb4c4fe1469e8d7c0f662437ac044
lyazj <seeson@pku.edu.cn>
2024-09-17 12:38:02 +0800

    Remove intermediate data

---
+++ plot_muemumu_points_file_ki.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+
+import ctypes
+import re
+import glob
+import ROOT
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+
+ki_points = glob.glob('data/muemumu_ki_*.root')
+ki_point_pattern = re.compile(r'muemumu_ki_([^_/]*)\.root$')
+
+# Load kinematics points.
+ki_point_index = { }  # [zp_mass][muon_energy]
+for ki_point in ki_points:
+    ki_point_match = ki_point_pattern.search(ki_point)
+    if not ki_point_match: continue
+    zp_mass = float(ki_point_match.group(1))
+    ki_point_index[zp_mass] = ki_point
+ki_points = sorted(ki_point_index.items())
+
+# Draw a plot for each (zp_mass, hist_name).
+for zp_mass, zp_mass_points in ki_points:
+    lc_index = { }  # [hist_name]
+    lc_meta = { }  # [hist_name]
+    print(zp_mass_points)
+    with ROOT.TFile.Open(zp_mass_points) as zp_mass_points:
+        lc_hists = { }  # [hist_name]
+        tree = zp_mass_points.Get('ki_points')
+        muon_energy_branch = np.empty(1, dtype='float64')
+        tree.SetBranchAddress('muon_energy', muon_energy_branch)
+        for branch in tree.GetListOfBranches():
+            if branch.GetTitle().endswith('/D'): continue
+            hist = ROOT.TH1D()
+            lc_hists[branch.GetName()] = hist
+            branch.SetAddress(ROOT.AddressOf(hist))
+        muon_energies = [ ]
+        ientry = 0
+        while tree.GetEntry(ientry):
+            ientry += 1
+            muon_energies.append(muon_energy_branch[0])
+            for hist_name, hist in lc_hists.items():
+                data = []
+                for i in range(1, hist.GetNbinsX() + 1): data.append([hist.GetBinCenter(i), hist.GetBinContent(i)])
+                if hist_name not in lc_index:
+                    lc_index[hist_name] = [ ]
+                    lc_meta[hist_name] = {
+                        'xtitle': hist.GetXaxis().GetTitle(),
+                        'ytitle': hist.GetYaxis().GetTitle(),
+                    }
+                lc_index[hist_name].append(np.array(data))
+        muon_energies = np.array(muon_energies)
+    for hist_name, lc in lc_index.items():
+        colors = plt.cm.viridis(np.linspace(0, 1, len(muon_energies)))
+        lc = LineCollection(lc, colors=colors)
+        plt.figure()
+        plt.gca().add_collection(lc)
+        plt.gca().autoscale()
+        plt.xlabel(lc_meta[hist_name]['xtitle'])
+        plt.ylabel(lc_meta[hist_name]['ytitle'])
+        cbar = plt.colorbar(lc)
+        tickposes = np.arange(len(muon_energies))[::10]
+        if len(tickposes) * 10 != len(muon_energies): tickposes = np.append(tickposes, len(muon_energies) - 1)
+        cbar.set_ticks(np.linspace(0, 1, len(tickposes)))
+        cbar.set_ticklabels(['%.2e' % f for f in muon_energies[tickposes]])
+        plt.tight_layout()
+        plt.savefig(hist_name + '_' + str(zp_mass) + '.pdf')
+        plt.close()
