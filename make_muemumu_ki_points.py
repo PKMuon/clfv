ac88318cd90a93c47d434411dba4f8731497f613
lyazj <seeson@pku.edu.cn>
2024-10-11 17:01:18 +0800

    Update zp mass range

--- make_muemumu_ki_points.py
+++ make_muemumu_ki_points.py
@@ -3,6 +3,7 @@
 import numpy as np
 from make_muemumu_point import make_muemumu_ki_point, zp_masses, muon_energies
 from MG5Run import pool
+zp_masses = np.linspace(0.01, 1.00, 100)
 
 args = [ ]
 for zp_mass in zp_masses:
